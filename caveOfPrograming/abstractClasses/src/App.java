3a1815ad6275076a365318327fe20d7abefb86a3
maisum <smmaisum@gmail.com>
2023-09-18 16:16:48 -0500

    Abstract Classes

---
+++ caveOfPrograming/abstractClasses/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        Camera camera = new Camera();
+        camera.setId(5);
+
+        Car car = new Car();
+        car.setId(4);
+    }
+}
---
+++ caveOfPrograming/abstractClasses/src/Machine.java
@@ -0,0 +1,13 @@
+public abstract class  Machine {
+    private int id;
+
+    // Methods
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
