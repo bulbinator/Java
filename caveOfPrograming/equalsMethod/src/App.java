537d1124f956f58fdca00ecf3da82a716c892a02
maisum <smmaisum@gmail.com>
2023-09-18 17:32:32 -0500

    Equals method

--- caveOfPrograming/equalsMethod/src/App.java
+++ caveOfPrograming/equalsMethod/src/App.java
@@ -4,7 +4,7 @@ public class App {
         Person person1 = new Person(2, "coomer");
         Person person2 = new Person(2, "coomer");
 
-        System.out.println(person1 == person2); // This will throw false because they are still different objects pointing at different locations in memory
+        System.out.println(person1== person2); // This will throw false because they are still different objects pointing at different locations in memory
 
         System.out.println(person1.equals(person2)); // In order to compare two objects, the objects must have thier own ".equals()" method defined in thier class, which can be done with the "Generate" option.
 
